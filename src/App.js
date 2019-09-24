04b79b469638dd08a6db93c77c40a8c440db5f4e
Andrew Murray <darbyguitars@gmail.com>
2019-09-24 10:34:04 -0500

    created the Nav component folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Nav from './Nav'
 
 function App() {
 	return (
-		<div className="App">
+    <div className="App">
+      <Nav />
 			<h1>Confetti Blog</h1>
 		</div>
 	)
---
+++ src/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Nav = () => {
+    return (
+        <h3>I am a nav bar!</h3>
+    )
+}
+
+export default Nav
