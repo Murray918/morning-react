743aa4a3d251c07275aab3da50fdee394c72e8c9
Andrew Murray <darbyguitars@gmail.com>
2019-09-24 11:16:55 -0500

    added the footer component imported the footer component into App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Nav from './Nav'
+import Footer from './Footer'
 
 function App() {
 	return (
     <div className="App">
       <Nav />
 			<h1>Confetti Blog</h1>
+      <Footer /> 
 		</div>
 	)
 }
---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+	render() {
+		return <h5 className="footer">i am a footer</h5>
+	}
+}
+
+export default Footer
